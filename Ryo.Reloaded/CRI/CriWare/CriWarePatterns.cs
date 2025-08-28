d1e58489262ad40a9651e5caa3e984dff5352ec4
RyoTune <zutomayofan50@gmail.com>
2025-08-27 20:52:59 -0500

    Remove unused patterns.

--- Ryo.Reloaded/CRI/CriWare/CriWarePatterns.cs
+++ Ryo.Reloaded/CRI/CriWare/CriWarePatterns.cs
@@ -14,6 +14,4 @@ public class CriWarePatterns
         };
 
     public string? HCADecoder_DecodeHeader { get; init; } = "48 8B C4 48 89 58 ?? 48 89 68 ?? 48 89 70 ?? 57 41 54 41 55 41 56 41 57 48 81 EC 90 00 00 00 4C 8D 70";
-    public string? criadxcodec_DecodeStream { get; init; } =  "48 8B C4 48 89 58 ?? 4C 89 48 ?? 55 56 57 41 54 41 55 41 56 41 57 48 83 EC 50";
-    public string? criAdxDecCore_SetEncryptionKey { get; init; } = "66 89 91 A2 ?? ?? ?? 66 44 89 81";
 }
